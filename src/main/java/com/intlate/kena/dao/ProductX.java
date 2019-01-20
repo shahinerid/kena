93846be65ea88532ecf8ec3ef2898f31024bfeb5
shahinerid <shahinerid@mds-macbook-pro.attlocal.net>
2019-01-19 22:29:08 -0500

    Commiting DAO files named as ..X

---
+++ src/main/java/com/intlate/kena/dao/ProductX.java
@@ -0,0 +1,16 @@
+package com.intlate.kena.dao;
+
+import java.util.List;
+
+import com.intlate.kena.entiti.Product;
+
+public class ProductX {
+
+	public static Product findById(String _id) {
+		return null;
+	}
+	
+	public static List<Product> findAll(){
+		return null;
+	}
+}
---
+++ src/main/java/com/intlate/kena/dao/SubCategoryX.java
@@ -0,0 +1,5 @@
+package com.intlate.kena.dao;
+
+public class SubCategoryX {
+
+}
